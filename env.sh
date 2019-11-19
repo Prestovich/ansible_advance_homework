7cf8327407f5e016039f839426c02991ae514002
root <root@bastion.1c71.example.opentlc.com>
2019-11-19 02:48:48 +0000

    syncing trials

---
+++ env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export TOWER_GUID=1c71
+export MYUSER=rpreston-redhat.com
+#export MYKEY=~/.ssh/otlckey
+export MYKEY=/root/.ssh/hwork.pem
+export OSP_GUID=be99
+export OPENTLC_LOGIN=rpreston-redhat.com
+export OPENTLC_PASSWORD=Ansibl3-l@b
+export GITHUB_REPO=https://github.com/Prestovich/ansible_advance_homework
+export JQ_REPO_BASE=http://www.opentlc.com/download/ansible_bootcamp
+export REGION=us-west-1
+export RH_MAIL_ID=rpreston@redhat.com
+
